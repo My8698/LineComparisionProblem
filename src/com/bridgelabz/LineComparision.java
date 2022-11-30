df35aeda3b7424db0027df7162dda84c99953857
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-11-30 11:51:01 +0530

    Added:compare two lines based on the endpoints

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -1,28 +1,40 @@
 package com.bridgelabz;
 import java.util.Scanner;
 public class LineComparision {
-    public static int LengthOfLine()
-    {
-        /**
-         * variables to take line co-ordinates
-         *
-         */
+    public static int lineLength() {
         int x1,y1,x2,y2;
-        Scanner scn=new Scanner(System.in);
-        System.out.println("Enter the Line co-ordinates->");
-        x1=scn.nextInt();
-        y1=scn.nextInt();
-        x2=scn.nextInt();
-        y2=scn.nextInt();
-        /**
-         * calculating length of line
-         */
-        int lineLength=(int)Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the line co-ordinates: x1,y1,x2,y2");
+        x1 = scn.nextInt();
+        y1 = scn.nextInt();
+        x2 = scn.nextInt();
+        y2 = scn.nextInt();
+        int lineLength = (int)Math.sqrt(Math.pow((x1-x2), 2)+ Math.pow((y1-y2), 2) );
         return lineLength;
     }
-    public static void main(String[] args)
-    {
-        int lineLength = LengthOfLine();
-        System.out.println("The length of line is: "+lineLength);
+
+    /**
+     * check equality of two lines
+     *by comparing two lines
+     * */
+    public static void LineEquality(int lineOneLength , int lineTwoLength ) {
+        if(lineOneLength == lineTwoLength ) {
+            System.out.println("Lines are Equal");
+        }
+        else if(lineOneLength > lineTwoLength){
+            System.out.println("Line 1 is greater than Line 2 ");
+        }
+        else {
+            System.out.println("Line 2 is greater than Line 1 ");
+        }
     }
+    public static void main(String[] args) {
+        int lineOneLength = lineLength();
+        int lineTwoLength = lineLength();
+        System.out.println("The length of line 1 is: "+lineOneLength);
+        System.out.println("The length of line 2 is: "+lineTwoLength);
+        LineEquality(lineOneLength,lineTwoLength);
+
+    }
+
 }
