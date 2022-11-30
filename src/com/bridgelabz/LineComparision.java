a6d7e0d7348cc90b8e2d394a70e17e253adba0e7
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-11-30 12:17:48 +0530

    Added:check equality of two lines based on line length

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -1,7 +1,8 @@
 package com.bridgelabz;
 import java.util.Scanner;
 public class LineComparision {
-    public static int lineLength() {
+    public static int lineLength()
+    {
         int x1,y1,x2,y2;
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter the line co-ordinates: x1,y1,x2,y2");
@@ -9,23 +10,17 @@ public class LineComparision {
         y1 = scn.nextInt();
         x2 = scn.nextInt();
         y2 = scn.nextInt();
-        int lineLength = (int)Math.sqrt(Math.pow((x1-x2), 2)+ Math.pow((y1-y2), 2) );
+        int lineLength = (int)Math.sqrt(Math.pow((x2-x1), 2)
+                + Math.pow((y2-y1), 2) );
         return lineLength;
     }
-
-    /**
-     * check equality of two lines
-     *by comparing two lines
-     * */
-    public static void LineEquality(int lineOneLength , int lineTwoLength ) {
+    public static void LineEquality(int lineOneLength , int lineTwoLength )
+    {
         if(lineOneLength == lineTwoLength ) {
             System.out.println("Lines are Equal");
         }
-        else if(lineOneLength > lineTwoLength){
-            System.out.println("Line 1 is greater than Line 2 ");
-        }
         else {
-            System.out.println("Line 2 is greater than Line 1 ");
+            System.out.println("Lines are not Equal");
         }
     }
     public static void main(String[] args) {
@@ -34,7 +29,5 @@ public class LineComparision {
         System.out.println("The length of line 1 is: "+lineOneLength);
         System.out.println("The length of line 2 is: "+lineTwoLength);
         LineEquality(lineOneLength,lineTwoLength);
-
     }
-
 }
